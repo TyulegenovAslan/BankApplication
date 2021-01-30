3676ea78c3539a9bd919739333c62b97b95848c6
AslanTyulegenov <tyulegenov1999@mail.ru>
2021-01-30 09:01:50 +0600

    Добавьте файлы проекта.

---
+++ BankLibrary/IAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLibrary
+{
+    interface IAccount
+    {
+        //Положить на счет
+        void Put(decimal sum);
+        //Списать со счета
+        decimal Withdrow(decimal sum);
+
+    }
+}
